a7f5e01f23dfc2e56190752ee42713d639f9b62d
Marzio Sala <msala>
2006-07-31 15:43:32 +0000

    Settings needed by step_process.py to locate the data repositories.

---
+++ common/config.py
@@ -0,0 +1,12 @@
+# All the settings below refer to MatrixPortal automatically
+# generated problems. They can be used to limit the problem size 
+# and prevent users to demand to build huge matrices, which 
+# may crash the server.
+MAX_MATRIX_ROWS = 256 * 256
+MAX_MATRIX_NONZEROS = 256 * 256 * 5
+MAX_ITERATIONS = 1550
+MAX_KSPACE = 60
+# This is the location of HB linear systems
+HB_REPOSITORY = "/var/www/html/WebTrilinos/data/HB/"
+# This is the location of XML linear systems
+XML_REPOSITORY = "/var/www/html/WebTrilinos/data/XML/"
